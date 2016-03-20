443dfd1fbff2e9160e09ae84e83b496af7ba2e5c
Paulo Tokimatu <paulotokimatu@users.noreply.github.com>
2016-03-20 19:42:00 -0300

    Update FB.py

--- FB.py
+++ FB.py
@@ -1,12 +1,19 @@
-### Next step: Use decorators
-
+def memoize(f):
+    cache = {}
+    def helper(x):
+        if x not in cache:
+            cache[x] = f(x)
+        return cache[x]
+    return helper
+    
+@memoize
 def fib(n):
     if (n == 1 or n == 2):
         return 1
     else:
         return fib(n-2)*k + fib(n-1)
 
-n = 30
+n = 40
 k = 3
 
 result = fib(n)
