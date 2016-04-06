1a941ea7c48a11505b5cfe04023572d4f9d6406d
Paulo Tokimatu <paulotokimatu@users.noreply.github.com>
2016-04-05 23:44:18 -0300

    Update 025_PROB.py

--- 025_PROB.py
+++ 025_PROB.py
@@ -1,14 +1,12 @@
-# To do: use property of logarithms in the hint
-
 import math
 
 def calc_prob(prob_cg, prob_at):
-    prob = 1
+    prob = 0
     for nuc in list(sequence):
         if nuc == "A" or nuc == "T":
-            prob *= prob_at
+            prob += math.log(prob_at, 10)
         elif nuc == "C" or nuc == "G":
-            prob *= prob_cg
+            prob += math.log(prob_cg, 10)
     return prob
 
 rosalind = open("rosalind_prob.txt", "r")
@@ -20,4 +18,4 @@ prob_list = file_lines[1].strip().split()
 for i in prob_list:
     prob_cg = float(i)/2
     prob_at = (1-float(i))/2
-    print ("%.3f" % math.log(calc_prob(prob_cg, prob_at), 10)),
+    print ("%.3f" % calc_prob(prob_cg, prob_at)),
